632d1aa346495f21eb37a4e18906e91c2316d6b4
Ahmed <ahmeddarm678@gmail.com>
2025-11-29 14:10:11 +0300

    "Fix-merge-conflicts"

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-=======
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -30,4 +21,3 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
->>>>>>> 566ea60 (أول دفعة، الله يستر)
